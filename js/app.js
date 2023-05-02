3338c543909c35dd5823a8bce1b637d300e957e3
Deba <debajyotipradhan0@gmail.com>
2023-05-02 20:27:22 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const endDate="3 May 2023 10:00 PM";
+
+document.getElementById("end-date").innerText= endDate;
+const inputs =  document.querySelectorAll("input")
+// initial call
+clock()
+
+function clock() {
+    const end = new Date(endDate);
+    const now = new Date();
+    // console.log(end);
+    // console.log(now);
+    const diff = (end-now) / 1000;
+
+    if (diff<0 ) return;
+    // convert into days
+    inputs[0].value= (Math.floor(diff /3600/24));
+    // convert into hours
+    inputs[1].value=(Math.floor(diff / 3600)%24);
+    // covert into minutes
+    inputs[2].value=(Math.floor(diff/60)%60);
+    // convert into seconds
+    inputs[3].value=(Math.floor(diff%60));
+}
+
+setInterval(
+    function(){
+        clock()
+    }
+    
+)
+   
+    
+
+
+
+
+// 1day=24hrs
+// 1hr=60min
+// 60min=3600 sec
