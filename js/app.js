661923fa632a06837814724761411a9b97b2efa5
Debajyoti Pradhan <105050817+githubbydeba@users.noreply.github.com>
2023-09-30 10:34:28 +0000

    app

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const endDate="3 May 2023 10:00 PM";
+const endDate="02 october 2023 10.00am";
 
 document.getElementById("end-date").innerText= endDate;
 const inputs =  document.querySelectorAll("input")
